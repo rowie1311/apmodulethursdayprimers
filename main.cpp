90d7651990ee1c6991768242ca835fc9ae3a1fd6
Rowan Priest <rowan.priest@ada.ac.uk>
2021-01-20 10:24:37 +0000

    completed, needs commenting

--- main.cpp
+++ main.cpp
@@ -4,14 +4,24 @@
 
 using namespace std;
 
-void sleepnow(int seconds) {
+void timer1(int seconds) {
   std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
+}
 
+void timer2(int seconds) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
 }
 
 void thread_function() {
   cout << "Thread: " << std::this_thread::get_id() << " started\n";
-  sleepnow(5);
+  timer1(5);
+  cout << "Thread: " << std::this_thread::get_id() << " ended\n";
+
+}
+
+void thread_function2() {
+  cout << "Thread: " << std::this_thread::get_id() << " started\n";
+  timer2(10);
   cout << "Thread: " << std::this_thread::get_id() << " ended\n";
 
 }
@@ -19,4 +29,9 @@ void thread_function() {
 int main() {
    cout << "Main Thread: " << std::this_thread::get_id() << " Started\n";
    std::thread t(&thread_function);
+   std::thread g(&thread_function2);
+   cout << "Main Thread: " << std::this_thread::get_id() << " Started\n";
+   t.join();
+   g.join();
+   cout << "Main Thread: " << std::this_thread::get_id() << " Ended\n";
 }
