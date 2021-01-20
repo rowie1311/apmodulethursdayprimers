0a4ccbc9bd4f1a882556aaf9742208c9525ce0d4
Rowan Priest <rowan.priest@ada.ac.uk>
2021-01-20 10:31:22 +0000

    completed, needs commenting

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include "thread"
+#include "chrono"
+#include <iostream>
+
+using namespace std;
+
+void timer1(int seconds) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
+}
+
+void timer2(int seconds) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
+}
+
+void thread_function() {
+  cout << "Thread: " << std::this_thread::get_id() << " started\n";
+  timer1(5);
+  cout << "Thread: " << std::this_thread::get_id() << " ended\n";
+
+}
+
+void thread_function2() {
+  cout << "Thread: " << std::this_thread::get_id() << " started\n";
+  timer2(10);
+  cout << "Thread: " << std::this_thread::get_id() << " ended\n";
+
+}
+
+int main() {
+   cout << "Main Thread: " << std::this_thread::get_id() << " Started\n";
+   std::thread t(&thread_function);
+   std::thread g(&thread_function2);
+   t.detach();
+   cout << "Main Thread: " << std::this_thread::get_id() << " Started\n";
+   if (t.joinable()){
+     t.join();
+   }
+   if (g.joinable()){
+     g.join();
+   }
+   cout << "Main Thread: " << std::this_thread::get_id() << " Ended\n";
+}
